c4b7f6963b7be133172cee8525758b3e337601c9
Baeed khan <baeedk7@gmail.com>
2024-09-10 13:18:28 +0500

    updating navbar

--- src/components/nav-component/nav.jsx
+++ src/components/nav-component/nav.jsx
@@ -1,16 +1,44 @@
-import react from "react";
-import { Link, } from "react-router-dom";
-import home from "./../../assets/home.png"
-import shop from "./../../assets/shop.png"
-import logo from "../.././assets/logo.png";
-import './nav.css'
+import React, { useState, useEffect } from 'react';
+import { Link, Outlet} from 'react-router-dom';
+import logo from './../../assets/logo.png'
+import './nav.css'; // Import the CSS file for styling
 
-function Navigation() {
+const Navbar = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setIsScrolled(true);
+        } else {
+            setIsScrolled(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        // Cleanup on component unmount
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
-        <>
+        <nav className={`navbar ${isScrolled ? 'scrolled' : ''}`}>
+                    <div className="nav">
+            <div className="logo"><img src={logo} alt="" /> </div>
+            <div className="menu" >
+           {/* <div className="menu-list"> <p><Link to='/'></Link></p></div> */}
+                <div className="menu-list"><p><Link to='/shop'>shop</Link></p></div>
+                <div className="menu-list"> <p><Link to= '/about'>About</Link></p></div>
+                 <div className="menu-list"><p><Link to= '/contact'>Contact</Link></p> </div>  
+            </div>
+            <div className="shop">
+                <Link to='/login'>
+                <button className="log">log in</button>
+                <button className="sign">sign up</button></Link>
+            </div>
+        </div>
+        {/* <Outlet/> */}
+        </nav>
+    );
+};
 
-
-        </>
-    )
-}
-export default Navigation;
+export default Navbar;
--- src/pages/header-page/header.jsx
+++ src/pages/header-page/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Link,Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Navbar from './../../components/nav-component/nav'
 import logo from "../../assets/logo.png";
 import './header.css';
 
@@ -7,20 +8,8 @@ function Nav() {
 
     return(
         <>
-        <div className="nav">
-            <div className="logo"><img src={logo} alt="" /> </div>
-            <div className="menu" >
-           {/* <div className="menu-list"> <p><Link to='/'></Link></p></div> */}
-                <div className="menu-list"><p><Link to='/shop'>shop</Link></p></div>
-                <div className="menu-list"> <p><Link to= '/about'>About</Link></p></div>
-                 <div className="menu-list"><p><Link to= '/contact'>Contact</Link></p> </div>  
-            </div>
-            <div className="shop">
-                <Link to='/login'>
-                <button className="log">log in</button>
-                <button className="sign">sign up</button></Link>
-            </div>
-        </div>
+<Navbar/>
+
         <Outlet/>
         {/* nav End */}
         
